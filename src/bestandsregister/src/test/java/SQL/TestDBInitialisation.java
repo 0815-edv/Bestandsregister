141b5dd30b0b7826311e0098536898437c9e8972
Markus <48181660+BackInBash@users.noreply.github.com>
2021-03-16 17:15:03 +0100

    Added Dummy UnitTest

---
+++ src/bestandsregister/src/test/java/SQL/TestDBInitialisation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Markus
+ */
+public class TestDBInitialisation {
+    @Test
+    public void TestDB(){
+        assertEquals(7, 7);
+    }
+}
