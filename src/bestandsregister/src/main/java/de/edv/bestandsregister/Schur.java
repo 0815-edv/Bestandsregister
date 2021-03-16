3e704eb98613a4b012ccf98e7e3a25de6d6020f3
Lorenz <lorenzfaber@gmail.com>
2021-03-16 15:57:45 +0100

    Update Schur.java
    added code to schur

--- src/bestandsregister/src/main/java/de/edv/bestandsregister/Schur.java
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/Schur.java
@@ -5,10 +5,28 @@
  */
 package de.edv.bestandsregister;
 
+import java.util.Date;
+
 /**
  *
  * @author lorenz
  */
 public class Schur {
+    private int schurID;
+    private int schafID;
+    private Date datum;
+    
+    public Schur(int schafID){
+        this.schafID = schafID;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+    
     
 }
