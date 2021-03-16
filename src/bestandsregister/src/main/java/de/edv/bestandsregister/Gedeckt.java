98e449da414aa39a1927ae63998cb461b5130bf1
Lorenz <lorenzfaber@gmail.com>
2021-03-16 15:32:49 +0100

    Create Gedeckt.java
    created gedeckt.java

---
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/Gedeckt.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.edv.bestandsregister;
+
+import java.util.Date;
+
+/**
+ *
+ * @author lorenz
+ */
+public class Gedeckt {
+    
+    private int gedecktID;
+    private int SchafID;
+    private Date datum;
+    private String vaterkennung;
+    
+    public Gedeckt(int schafID){
+        this.SchafID = schafID;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public String getVaterkennung() {
+        return vaterkennung;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public void setVaterkennung(String vaterkennung) {
+        this.vaterkennung = vaterkennung;
+    }
+    
+    
+    
+}
