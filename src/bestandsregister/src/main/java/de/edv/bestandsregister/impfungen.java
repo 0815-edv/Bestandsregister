fa2564b9cd1a8a9efbdf6508e990379bc3ce8f0d
Lorenz <lorenzfaber@gmail.com>
2021-03-16 15:26:09 +0100

    Update impfungen.java
    created datatypes, getter, setter and the method

--- src/bestandsregister/src/main/java/de/edv/bestandsregister/impfungen.java
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/impfungen.java
@@ -5,10 +5,48 @@
  */
 package de.edv.bestandsregister;
 
+import java.util.Date;
+
 /**
  *
  * @author lorenz
  */
-public class impfungen {
-    //test
+public class Impfungen {
+    int schafID;
+    int ImpfID;
+    String impfstoff;
+    String bemerkung;
+    Date datum;
+    
+    public void Impfungen(int SchafID){
+       //here the code for the method "impfungen" 
+    }
+
+    public int getImpfID() {
+        return ImpfID;
+    }
+
+    public String getBemerkung() {
+        return bemerkung;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setImpfstoff(String impfstoff) {
+        this.impfstoff = impfstoff;
+    }
+
+    public void setBemerkung(String bemerkung) {
+        this.bemerkung = bemerkung;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+    
+    
+    
+    
 }
