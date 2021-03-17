4d096a160c0d2f436ebc213fb01eefb614e3d873
Markus <48181660+BackInBash@users.noreply.github.com>
2021-03-17 14:32:34 +0100

    Added DB Init UnitTest

--- src/bestandsregister/src/test/java/SQL/TestDBInitialisation.java
+++ src/bestandsregister/src/test/java/SQL/TestDBInitialisation.java
@@ -5,16 +5,56 @@
  */
 package SQL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
 import org.junit.jupiter.api.Test;
+import de.edv.bestandsregister.SQL.*;
+import de.edv.bestandsregister.Schaf;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
  * @author Markus
  */
 public class TestDBInitialisation {
+
+    /**
+     * Test Creation of the SQLite Database File
+     */
     @Test
-    public void TestDB(){
-        assertEquals(7, 7);
+    public void TestDB() {
+        // Test IF Fiele Exists
+        File f = new File("register.db");
+        if (!f.exists() && !f.isDirectory()) {
+            Add insert = new Add();
+            insert.schaf(new Schaf() {
+            });
+        }
+
+        ArrayList<String> tables = new ArrayList<String>();
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:register.db");
+                Statement stmt = conn.createStatement()) {
+            String s = "SELECT name FROM sqlite_master WHERE type='table';";
+
+            ResultSet rs = stmt.executeQuery(s);
+
+            while (rs.next()) {
+                tables.add(rs.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            assertFalse(true);
+        }
+
+        assertTrue(tables.size() == 9 && f.exists());
+        f.delete();
     }
 }
