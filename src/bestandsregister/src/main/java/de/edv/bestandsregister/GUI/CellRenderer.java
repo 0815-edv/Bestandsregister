6749aaab3cc4c3732e7b12db366a576b19b0cd4d
Markus <48181660+BackInBash@users.noreply.github.com>
2021-04-19 17:45:25 +0200

    Added Comments

--- src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/CellRenderer.java
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/CellRenderer.java
@@ -18,25 +18,37 @@ import javax.swing.JList;
 public class CellRenderer extends DefaultListCellRenderer {
 
     @Override
+    /**
+     * Custom Cell Renderer
+     * Um die Einträge der Übersichts JListe optimal entsprechend
+     * des Bestands entsprechend als aktiv oder inaktiv zu setzen.
+     */
     public Component getListCellRendererComponent(JList list, Object value, int index,
             boolean isSelected, boolean cellHasFocus) {
         Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         if (value instanceof Schaf) {
             Schaf schaf = (Schaf) value;
+            // Setze Anzeige Text
             setText(schaf.toString());
+            
             if (schaf.getDatumAbgang() != null) {
                 if (schaf.getDatumAbgang().getTime() != 0) {
+                    // Setzte Abgangs Kennzeichnung
                     setBackground(Color.RED);
                 } else {
+                    // Setze Bestands Kennzeichnung
                     setBackground(Color.GREEN);
                 }
             } else {
+                // Setze Bestandskennzeichnung wenn Datum NULL
                 setBackground(Color.GREEN);
             }
             if (isSelected) {
+                // Hebt das Aktiv ausgewählte Item hervor
                 setBackground(getBackground().darker());
             }
         } else {
+            // Fehler Objekt ist nicht vom Typ Schaf
             setText("Unknown Type?");
         }
         return c;
--- src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/SortByAbgang.java
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/SortByAbgang.java
@@ -15,22 +15,43 @@ import java.util.Comparator;
 public class SortByAbgang implements Comparator<Schaf> {
 // Used for sorting in ascending order of ID  
 
+    /**
+     * Dient zur Sortierung der Übersichts JListe
+     * von Aktiv und Inaktiven Einträgen.
+     * @param schafA
+     * @param schafB
+     * @return 
+     */
     public int compare(Schaf schafA, Schaf schafB) {
+        // Initialisiere mit Inaktiv
         int a = 0;
         int b = 0;
 
         if (schafA.getDatumAbgang() != null) {
             if (schafA.getDatumAbgang().getTime() != 0) {
+                // Setze Aktiv
                 a = 1;
             }
         }
 
         if (schafB.getDatumAbgang() != null) {
             if (schafB.getDatumAbgang().getTime() != 0) {
+                // Setze Aktiv
                 b = 1;
             }
         }
-
+        /**
+         * Berechne Differenz
+         * Bsp1. 
+         * SchafA -> Aktiv (1)
+         * SchafB -> Inaktiv (0)
+         * 1-0 = 0 (status unverändert)
+         * 
+         * Bsp2.
+         * SchafA -> Inaktiv (0)
+         * SchafB -> Aktiv (1)
+         * 0-1 = -1 (SchafB wird nach oben in der Liste verschoben)
+         */
         return a - b;
     }
 }
