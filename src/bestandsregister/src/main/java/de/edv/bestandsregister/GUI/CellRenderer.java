f5b98449a6e33827364c84e77f021559c6fffd0e
Markus <48181660+BackInBash@users.noreply.github.com>
2021-04-30 13:11:34 +0200

    Updated License Headers

--- src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/CellRenderer.java
+++ src/bestandsregister/src/main/java/de/edv/bestandsregister/GUI/CellRenderer.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * MIT License
+ *
+ *  Copyright (c) 2021 Markus, Lorenz, flwerner
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 package de.edv.bestandsregister.GUI;
 
@@ -19,9 +37,8 @@ public class CellRenderer extends DefaultListCellRenderer {
 
     @Override
     /**
-     * Custom Cell Renderer
-     * Um die Einträge der Übersichts JListe optimal entsprechend
-     * des Bestands entsprechend als aktiv oder inaktiv zu setzen.
+     * Custom Cell Renderer Um die Einträge der Übersichts JListe optimal
+     * entsprechend des Bestands entsprechend als aktiv oder inaktiv zu setzen.
      */
     public Component getListCellRendererComponent(JList list, Object value, int index,
             boolean isSelected, boolean cellHasFocus) {
@@ -30,7 +47,7 @@ public class CellRenderer extends DefaultListCellRenderer {
             Schaf schaf = (Schaf) value;
             // Setze Anzeige Text
             setText(schaf.toString());
-            
+
             if (schaf.getDatumAbgang() != null) {
                 if (schaf.getDatumAbgang().getTime() != 0) {
                     // Setzte Abgangs Kennzeichnung
